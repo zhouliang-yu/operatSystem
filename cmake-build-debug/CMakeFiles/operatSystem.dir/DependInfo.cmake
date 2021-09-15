a59c989151cde7e646b9b1b8dc47cee1e8c047f4
zhouliang-yu <120040077@link.cuhk.edu.cn>
2021-09-15 18:27:43 +0800

    add csc3150 tut1

---
+++ cmake-build-debug/CMakeFiles/operatSystem.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/d/operatSystem/a.c" "/mnt/d/operatSystem/cmake-build-debug/CMakeFiles/operatSystem.dir/a.c.o"
+  "/mnt/d/operatSystem/ctu.c" "/mnt/d/operatSystem/cmake-build-debug/CMakeFiles/operatSystem.dir/ctu.c.o"
+  "/mnt/d/operatSystem/lab0.c" "/mnt/d/operatSystem/cmake-build-debug/CMakeFiles/operatSystem.dir/lab0.c.o"
+  "/mnt/d/operatSystem/printBytes.c" "/mnt/d/operatSystem/cmake-build-debug/CMakeFiles/operatSystem.dir/printBytes.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
