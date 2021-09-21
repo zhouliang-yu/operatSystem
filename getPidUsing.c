71e666e4391943ce9023419849b821eb1d47874b
zhouliang-yu <120040077@link.cuhk.edu.cn>
2021-09-21 19:05:05 +0800

    finish getUsingPid

--- getPidUsing.c
+++ getPidUsing.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 
 int main(int argc, char* argv[]) {
@@ -10,5 +11,11 @@ int main(int argc, char* argv[]) {
         sleep(1);
     }
     printf("current id: %d, parent id: %d\n ",getpid(), getppid());
+    int res = wait(NULL);
+    if (res == -1) {
+        printf("no children to wait for \n");
+    }else{
+        printf("%d finished execution\n", res);
+    }
     return 0;
 }
