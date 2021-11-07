79c6b44aa84f23cb1607ed04a3321b3fc657766b
zhouliang-yu <120040077@link.cuhk.edu.cn>
2021-11-07 21:45:09 +0800

    start ass3

--- Assignment_3_120040077/virtual_memory.cu
+++ Assignment_3_120040077/virtual_memory.cu
@@ -32,13 +32,90 @@ __device__ void vm_init(VirtualMemory *vm, uchar *buffer, uchar *storage,
   init_invert_page_table(vm);
 }
 
-__device__ uchar vm_read(VirtualMemory *vm, u32 addr) {
-  /* Complate vm_read function to read single element from data buffer */
-  return 123; //TODO
+__device__ bool check_page_fault(VirtualMemory *vm, u32 addr, u32 page_num) {
+  for (int i = 0; i < vm -> PAGE_ENTRIES; i ++) {
+    if (vm -> invert_page_table[i] == page_num) {
+      return true;
+    }
+  }
+  *vm->pagefault_num_ptr = *vm->pagefault_num_ptr + 1;
+  return false;
 }
+
+__device__ void move_to_memory(VirtualMemory *vm, u32 frame_num, u32 page_num) {
+  u32 original_page_num = vm->invert_page_table[frame_num];
+  for (int i = 0; i < 32; i ++) {
+    vm->storage[original_page_num * 32 + i] = vm -> buffer[frame_num * 32 + i];
+    vm->buffer[frame_num * 32 + i] = vm -> storage[page_num * 32 + i];
+  }
+  vm -> invert_page_table[frame_num] = page_num;
+}
+
+__device__ int find_frame_number(VirtualMemory *vm, u32 page_num) {
+  for (int i = 0; i < vm -> PAGE_ENGTRIES; i++) {
+    if (vm -> invert_page_table[i] == page_num) {
+      return i;
+    }
+  }
+  printf("fault: get out of the index range");
+  return -1;
+}
+
+__device__ int find_frame_num_in_frame_table(VirtualMemory *vm, u32 frame_num)
+{
+  for (int i = 0; i < vm -> PAGE_ENTRIES; i++){
+    if (vm -> invert_page_table[i + vm ->PAGE_ENTRIES] == frame_num){
+      return i;
+    }
+  }
+  printf("fault: get out of the index range");
+  return -1;
+}
+
+__device__ void change_frame_table_valid_to_invalid(VirtualMemory *vm, u32 frame_num) {
+  int tempt = vm->invert_page_table[vm->PAGE_ENTRIES + find_frame_num_in_frame_table(vm, frame_num);
+  for (int i = find_frame_num_in_frame_table(vm, frame_num); i < vm -> PAGE_ENTRIES - 1; i ++) {
+    vm->invert_page_table[i + vm->PAGE_ENTRIES] = vm->invert_page_table[i + vm->PAGE_ENTRIES + 1];
+  }
+  vm -> invert_page_table[2 * vm->PAGE_ENTRIES - 1] = tempt;
+}
+
+__device__ uchar vm_read(VirtualMemory *vm, u32 addr)
+{
+  /* Complate vm_read function to read single element from data buffer */
+  u32 page_num = addr / 32;
+  u32 page_offset = addr % 32;
+  u32 frame_num; 
+
+  if (!check_page_fault(vm, page_num)) {
+    frame_num = vm -> invert_page_table[vm->PAGE_ENTRIES];
+    move_to_memory(vm, frame_num, page_num);
+  }else {
+    frame_num = find_frame_number(vm, page_num);
+  }
+  change_frame_table_valid_to_invalid(vm, frame_num);
+}
+
+
+
+
+
 
 __device__ void vm_write(VirtualMemory *vm, u32 addr, uchar value) {
   /* Complete vm_write function to write value into data buffer */
+  u32 page_num = addr / 32;
+  u32 page_offset = addr % 32;
+  u32 frame_num;
+  if(!check_page_fault(vm, page_num)) {
+    frame_num = vm->invert_page_table[vm->Page_ENTRIES];
+    check_frame_full(vm,page_num,frame_num);
+    vm->invert_page_table[frame_num] = page_num;
+  }
+  else{
+    frame_num = find_frame_number(vm, page_num);
+  }
+  vm->buffer[frame_num * 32 + page_offset] = value;
+  change_frame_table_valid_to_invalid(vm, frame_num);
 }
 
 __device__ void vm_snapshot(VirtualMemory *vm, uchar *results, int offset,
@@ -46,4 +123,5 @@ __device__ void vm_snapshot(VirtualMemory *vm, uchar *results, int offset,
   /* Complete snapshot function togther with vm_read to load elements from data
    * to result buffer */
 }
+
 
