24d0efa226a913a63eac039b4a79a5273a67da82
zhouliang-yu <120040077@link.cuhk.edu.cn>
2021-11-17 11:44:25 +0800

    init ass4

---
+++ Assignment_3_120040077/bonus/bonus_main.cu
@@ -0,0 +1,148 @@
+#include "virtual_memory.h"
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+#define DATAFILE "./data.bin"
+#define OUTFILE "./snapshot.bin"
+
+// page size is 32bytes
+#define PAGE_SIZE (1 << 5)
+// 16 KB in page table
+#define INVERT_PAGE_TABLE_SIZE (1 << 14)
+// 32 KB in shared memory
+#define PHYSICAL_MEM_SIZE (1 << 15)
+// 128 KB in global memory
+#define STORAGE_SIZE (1 << 17)
+
+#define LOCK(); for(int p=0;p<4;p++){if(threadIdx.x==p){
+#define UNLOCK(); }__syncthreads();}
+//// count the pagefault times
+__device__ __managed__ int pagefault_num = 0;
+
+// data input and output
+__device__ __managed__ uchar results[STORAGE_SIZE];
+__device__ __managed__ uchar input[STORAGE_SIZE];
+
+// memory allocation for virtual_memory
+// secondary memory
+__device__ __managed__ uchar storage[STORAGE_SIZE];
+// page table
+extern __shared__ u32 pt[];
+
+__device__ void user_program(VirtualMemory *vm, uchar *input, uchar *results,
+                             int input_size);
+__device__ void init_invert_page_table(VirtualMemory *vm);
+__global__ void mykernel(int input_size) {
+
+  // memory allocation for virtual_memory
+  // take shared memory as physical memory
+  __shared__ uchar data[PHYSICAL_MEM_SIZE];
+
+  VirtualMemory vm;
+  vm_init(&vm, data, storage, pt, &pagefault_num, PAGE_SIZE,
+          INVERT_PAGE_TABLE_SIZE, PHYSICAL_MEM_SIZE, STORAGE_SIZE,
+          PHYSICAL_MEM_SIZE / PAGE_SIZE);
+  if(threadIdx.x == 0) {
+    init_invert_page_table(&vm);
+  }
+
+ /** calling the __synthreads() instrincfunction to specify the synchronization points in the kernel
+  * __synthreads acts as barrier at which threads in the block must wait before any is allowed to proceed*/
+  LOCK();
+
+  // call the write function
+  for (int i = 0; i < input_size; i++) {
+    vm_write(&vm, i, input[i]);
+  }
+  UNLOCK();
+
+  //read
+  for (int i = input_size - 1; i >= input_size - 32769; i--){
+    LOCK();
+    int value = vm_read(&vm, i);
+    UNLOCK();
+  }
+
+  LOCK();
+  vm_snapshot(&vm, results, 0, input_size);
+  UNLOCK();
+  return;
+}
+
+__host__ void write_binaryFile(char *fileName, void *buffer, int bufferSize) {
+  FILE *fp;
+  fp = fopen(fileName, "wb");
+  fwrite(buffer, 1, bufferSize, fp);
+  fclose(fp);
+}
+
+__host__ int load_binaryFile(char *fileName, void *buffer, int bufferSize) {
+  FILE *fp;
+
+  fp = fopen(fileName, "rb");
+  if (!fp) {
+    printf("***Unable to open file %s***\n", fileName);
+    exit(1);
+  }
+
+  // Get file length
+  fseek(fp, 0, SEEK_END);
+  int fileLen = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+
+  if (fileLen > bufferSize) {
+    printf("****invalid testcase!!****\n");
+    printf("****software warrning: the file: %s size****\n", fileName);
+    printf("****is greater than buffer size****\n");
+    exit(1);
+  }
+
+  // Read file contents into buffer
+  fread(buffer, fileLen, 1, fp);
+  fclose(fp);
+
+  return fileLen;
+}
+
+int main() {
+  clock_t t;
+  t = clock();
+
+  cudaError_t cudaStatus;
+  int input_size = load_binaryFile(DATAFILE, input, STORAGE_SIZE);
+  
+  
+  /* Launch kernel function in GPU, with single thread
+  and dynamically allocate INVERT_PAGE_TABLE_SIZE bytes of share memory,
+  which is used for variables declared as "extern __shared__" */
+
+  
+  mykernel<<<1, 4, INVERT_PAGE_TABLE_SIZE>>>(input_size/4);
+  
+  cudaStatus = cudaGetLastError();
+  if (cudaStatus != cudaSuccess) {
+    fprintf(stderr, "mykernel launch failed: %s\n",
+            cudaGetErrorString(cudaStatus));
+    return 0;
+  }
+
+  printf("input size: %d\n", input_size);
+
+  cudaDeviceSynchronize();
+  cudaDeviceReset();
+
+  write_binaryFile(OUTFILE, results, input_size);
+
+  t = clock() - t;
+  printf("the times of page table is:%f\n", ((float)t) / CLOCKS_PER_SEC);
+  printf("pagefault number is %d\n", pagefault_num);
+
+  return 0;
+}
